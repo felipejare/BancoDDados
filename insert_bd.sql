7a3860219a87762117bda9e3b427f4ada8919188
Felipe "Felepopolo Jaré" Kuhl <99222712+felipejare@users.noreply.github.com>
2022-08-31 15:12:37 -0300

    Add files via upload

--- insert_bd.sql
+++ insert_bd.sql
@@ -1,4 +1,98 @@
 /*
-insert into comenta(cod_postagem, cod_usuario, dathora_coment, texto_coment)
-(101, 01, 14:04, "muito pica")
-insert into
+insert into comenta
+(00535, 11118, 06/1313, "carambolas muito estupefato"),
+(09121, 22227, 15/2323, "meu deus me ayuda"),
+(91234, 33337, 30/2331, "sou o crente mais foda"),
+(83123, 44441, 21/1404, "time stamp"),
+(12342, 55552, 23/1505, "amo mysql"),
+(04935, 66663, 13/2034, "amo minha vida"),
+(41244, 77775, 17/1032, "saas"),
+(12313, 88883, 18/0932, "meu deus minha maoaaa"),
+(12314, 99992, 19/0543, "meu deus to muito felix"),
+(12513, 10004, 31/0454, "ta muito tardekkkk");
+insert into postagem
+("cachorro lindo", "animal", 03241, "muito massa essa imagem", 00453),
+("praia linda", "local", 03421, "muito linda essa praia", 00543),
+("casa linda", "local", 04234, "muito linda casa", 00423),
+("predio feia", "local", 04234, "muito feio esse predio naum gostey", 00342)
+("pessoa jurudica", "humano", 03241, "nao gosto de pessoa juridica >:c"),
+("comida estupefata", "alimento", 03242, "tava muito bom esse alimento eu amei cinco estrelas", 00242),
+("carros rapidos", "carros", 03242, "carros rapidos muitas gatas yo minha cama ta lotada to cheio de grana", 00232),
+("musico cantor", "musicos", 02123, "cara, cara, mlk uso drogas, mlk fumo elas, to fumando não me enche", 00321),
+("arcondicionado", "eletronicos", 06123, "estupefatos você nao poderá me impedir estupefatos nao da par me impedir", 00456),
+("yung lixo", "cantores", 06235, "É A POTENCIA NÃO TEM COMO MLK NAO SE METE E A POTENCIA NÃO TEM COMO POR FAVOR NAO SE METE E A POTENCIA COM LICENCA FUMA MACONHA É A POTENCIA", 00756);
+insert into doacao
+(001, 00321, 05/1405, 10.50),
+(002, 00563, 02/1604, 32.21),
+(003, 00423, 04/1234, 02.31),
+(004, 00622, 06/2341, 14.05),
+(005, 00724, 07/1254, 18.50),
+(006, 00862, 10/1905, 25.50),
+(007, 00842, 10/1602, 31.05),
+(008, 00142, 31/0723, 02.45),
+(009, 00421, 21/1805, 14.05),
+(010, 00789, 01/1704, 16.50);
+inser into usuario
+(18, "arrombado", "emaildoarombado@gmail.com", 00541, 12314124, 02),
+(23, "carminha", "caminhalinda@gmail.com", 00983, 42323202, 02),
+(21, "nogueira", "nogueiradabike@gmail.com", 00894, 32324141, 02),
+(32, "otaldochefe", "otaldodochefe@gmail.com", 00423, 33231231, 01),
+(34, "otaldocliente", "soucliente@gmail.com", 00487, 11223344, 02),
+(23, "ritalee", "ritaleeoficial@gmail.com", 00834, 98762345, 02),
+(76, "gilbertogil", "gilbertogil@gmail.com", 00234, 87341345, 01),
+(12, "renatorusso", "renatorusso@gmail.com", 00254, 97651234, 02),
+(36, "elisregina", "elisregina@gmail.com", 00345, 87632543, 02),
+(27, "yunglixo", "yunglixo@gmail.com", 00873, 97656563, 02);
+insert into tipo_usuario
+(01, "adm"),
+(02, "cliente");
+insert into curtir
+(29475, 19384, 13/1345), 
+(23468, 28274, 04/1406), 
+(74628, 33333, 31/1233), 
+(92737, 48274, 30/1855), 
+(08374, 53764, 25/1734), 
+(72644, 67347, 06/1632), 
+(02374, 79284, 20/2108), 
+(92847, 89287, 17/1946), 
+(98374, 98275, 16/3109), 
+(93764, 18476, 25/2611);
+insert into midia
+("cachorro", "batata", 18347, 92745),
+("praia", "lentidao", 29348, 93846),
+("casa", "besteira", 39856, 94867),
+("predio", "jesus", 49485, 38474),
+("pessoa", "jesus", 59485, 83743),
+("comida", "gilberto", 69386, 72445),
+("carro", "maca", 78456, 83743),
+("musico", "lama", 83645, 94735),
+("ar", "carambola", 92845, 82745),
+("gemaplys", "gilberto", 19375, 82743);
+insert into ong
+("lamacenta@email.com", 92848244, "lamacenta", 001),
+("grama@email.com", 87549340, "grama", 002),
+("cachorros@email.com", 92347547, "cachorros", 003),
+("criancas@email.com", 92836235, "crianças", 004),
+("animais@email.com", 92846264, "animais", 005),
+("internet@email.com", 98271234, "internet", 006),
+("amor@email.com", 83749284, "amor", 007),
+("elegante@email.com", 92486432, "elegantes", 008),
+("miopia@email.com", 92842365, "miopes", 009),
+("astigmatismo@email.com", 83756543, "pessoa com astigmatismo", 010);
+insert into voluntario
+(19365, 92735, 09/1155),
+(28456, 23653, 25/1845),
+(34854, 48564, 07/1608),
+(49354, 92343, 18/1809),
+(59245, 82743, 31/2350),
+(69245, 92843, 30/2140),
+(79353, 92352, 14/1414),
+(89245, 92743, 15/1212),
+(99274, 82356, 11/1309),
+(10345, 92374, 05/1408);
+
+
+
+
+
+
